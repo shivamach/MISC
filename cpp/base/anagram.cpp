cc0370526e8afb9b99901d343fa7940e5fbc8d32
shivamach <shivamach13@gmail.com>
2020-05-12 16:49:19 +0530

    still working on anagram code, others are complete

---
+++ cpp/base/anagram.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cstdio>
+
+using namespace std;
+
+int main(){
+char alphabets[5] = {'a','b','c','d','e'};
+for(int i=0;i<5;i++){
+printf("%d\n",alphabets[i]);}
+return 0;
+}
---
+++ cpp/base/scope.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+//ain't this cool???
+int main(){
+int i=100,sum=0;
+for (int i;i != 10;++i){
+sum += i;}
+cout << i << " " << sum << endl;
+return 0;
+}
